ad7377cc460ebb42df825dc2df2dcd0abe264ec2
paul <paul.sajo.official@gmail.com>
2022-09-12 13:30:04 +0530

    minor and major changes

--- parent/urls.py
+++ parent/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 
 urlpatterns = [
-    path('talk/<path:talk>/', views.talk_view, name='talk'),
+    path('talk/', views.talk_view, name='talk'),
     path('train/', views.train_data_view, name='talk')
 ]
--- parent/views.py
+++ parent/views.py
@@ -3,9 +3,9 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser
 
-
 from chatterbot import ChatBot  
 from chatterbot.trainers import ListTrainer  
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
 
 from .models import TrainingConversation
@@ -15,23 +15,32 @@ vp_bot = ChatBot(
     name = 'Sara',  # semi-automated-robotic-AI
     read_only = True,  
     logic_adapters = [  
-        'chatterbot.logic.MathematicalEvaluation',  
+        # 'chatterbot.logic.MathematicalEvaluation',  
         'chatterbot.logic.BestMatch'  
-        ]  
+        ],
+              
 )  
+
+trainer = ChatterBotCorpusTrainer(vp_bot)
+
+trainer.train('chatterbot.corpus.english')
+
+# Now we can export the data to a file
+trainer.export_for_training('./my_export.json')
 
 list_trainee = ListTrainer(vp_bot)  
 
 
-@api_view(['GET'])
-def talk_view(request, talk):
-    return Response({"message": vp_bot.generate_response(talk)})
+@api_view(['POST'])
+def talk_view(request):
+    print("Requets: ", vp_bot.get_response("hello"))
+    return Response({"message": str(vp_bot.get_response(request.data.get('text')))})
 
 
 @api_view(['GET'])
-@permission_classes(IsAdminUser)
+# @permission_classes([IsAdminUser])
 def train_data_view(request):
 
-    list_trainee.train(TrainingConversation.objects.all())  
+    list_trainee.train([x.data for x in TrainingConversation.objects.all()])  
     
     return Response(status=status.HTTP_200_OK)
