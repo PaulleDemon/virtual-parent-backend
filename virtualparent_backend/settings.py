c778873221c96a1f5b42fb439230768b428d8391
paul <paul.sajo.official@gmail.com>
2022-09-25 15:20:31 +0530

    minor updates to settings

--- virtualparent_backend/settings.py
+++ virtualparent_backend/settings.py
@@ -74,7 +74,7 @@ ROOT_URLCONF = 'virtualparent_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR.joinpath("build"),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -136,6 +136,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR.joinpath('static')
+
+STATICFILES_DIRS = [
+                        BASE_DIR.joinpath('build'),
+                        ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
