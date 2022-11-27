ed7463c12fb95686122cc4df24cde71033bdd789
paul <paul.sajo.official@gmail.com>
2022-11-28 04:34:47 +0530

    minor updates

--- virtualparent_backend/urls.py
+++ virtualparent_backend/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('parent/', include('parent.urls'))
 ]
+
+if not settings.DEBUG:
+    urlpatterns += [re_path(r'(?P<path>,*)', TemplateView.as_view(template_name='index.html'))]
